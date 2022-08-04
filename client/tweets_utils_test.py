093ab89ff56eb4176dc2b97d171606dc42684fc3
Palermo Macbook Air <pspenano@outlook.com>
2022-08-03 23:39:17 -0400

    add some manual testing of Twitter's upload media endpoint

--- client/tweets_utils_test.py
+++ client/tweets_utils_test.py
@@ -1,7 +1,11 @@
+import os.path
+
 import tweepy
 import tweets_utils
 import yaml
 
+import importlib
+importlib.reload(tweets_utils)
 
 with open("../config.yml", "r") as stream:
 	try:
@@ -19,11 +23,22 @@ client = tweepy.Client(
 )
 
 most_recent_text_prompt = tweets_utils.retrieve_most_recent_text_prompt(client=client, configs=configs)
-
 print(most_recent_text_prompt)
 
 # upload to twitter
 # Get id of most latest tweet
 # Reply to most latest tweet with attached image
+#client.create_tweet(text='Some reply', in_reply_to_tweet_id=1554678399125360640)
 
-client.create_tweet(text='Some reply', in_reply_to_tweet_id=1554678399125360640)
+
+# Twitter API V1
+
+auth = tweepy.OAuthHandler(twitter_api_keys["consumer_key"], twitter_api_keys["consumer_secret"])
+auth.set_access_token(twitter_api_keys["access_token_key"], twitter_api_keys["access_token_secret"])
+api = tweepy.API(auth)
+
+#api.update_status("test123", in_reply_to_status_id=1554678399125360640)
+saved_image_folder = 'saved_images'
+image_path = os.path.join(saved_image_folder, 'pearl_earings.jpg')
+print(type(image_path), image_path)
+api.update_status_with_media("", filename=image_path , in_reply_to_status_id=1554678399125360640)
