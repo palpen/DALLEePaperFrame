631abe9ea1fcbe129cbd9c0dc312200a98c7f665
Palermo Penano <pspenano@yelp.com>
2022-08-05 22:52:52 -0400

    revert run_server.sh back to its original command

--- server/run_server.sh
+++ server/run_server.sh
@@ -1,3 +1,2 @@
-DOCKER_COMPOSE_FILE=$1
-docker-compose --file $DOCKER_COMPOSE_FILE up --abort-on-container-exit --remove-orphans
+docker-compose up --abort-on-container-exit --remove-orphans
 #docker-compose up -d --remove-orphans
