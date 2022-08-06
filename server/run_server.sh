2601df15afe2d6226e0f1aca9707bc3044b953ee
pspenano <pspenano@outlook.com>
2022-08-05 22:32:58 -0400

    add argument for path to the docker compose yaml file

--- server/run_server.sh
+++ server/run_server.sh
@@ -1,2 +1,3 @@
-docker-compose up --abort-on-container-exit --remove-orphans
+DOCKER_COMPOSE_FILE=$1
+docker-compose --file $DOCKER_COMPOSE_FILE up --abort-on-container-exit --remove-orphans
 #docker-compose up -d --remove-orphans
