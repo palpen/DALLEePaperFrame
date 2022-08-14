228cc5e5faf54380224b3727f2b6ff2bf2ffe462
Palermo Penano <pspenano@outlook.com>
2022-08-14 03:09:50 +0000

    Remove credit (this will be placed on the actual frame that the display will be mounted in instead); organize code

--- client/frame_composer.py
+++ client/frame_composer.py
@@ -3,43 +3,56 @@ from PIL import Image, ImageDraw, ImageFont
 
 class FrameComposer:
     def __init__(self, width, height):
+
+        # Display dimensions
         self.width = width
         self.height = height
-        self.large_font_height = 25
-        self.small_font_height = 15
+
+        # Text prompt properties
+        self.large_font_height = 22
         self.large_font = ImageFont.truetype("/usr/share/fonts/truetype/msttcorefonts/arial.ttf",
                                              self.large_font_height)
-        self.small_font = ImageFont.truetype("/usr/share/fonts/truetype/msttcorefonts/arial.ttf",
-                                             self.small_font_height)
 
-        self.image_y = 25
-        self.border_width = 5
-
-        self.credit_text = "by: Dalle Mini, an AI"
+        # Image border properties
+        self.image_y = 13  # Vertical distance between top of display and top image border
+        self.border_width = 7
 
     def create_frame_image(self, image, text_prompt):
-        base_image = Image.new("RGB", (self.width, self.height), "white")
+        """Generates the layout to display on the frame
 
+        To quickly iterate on different types of layouts, see frame_composer_test.py
+        """
+        base_image = Image.new("RGB", (self.width, self.height), "white")
         draw = ImageDraw.Draw(base_image)
 
-        # add text
-        draw.rectangle(((self.width - image.width) // 2 - self.border_width,
-                        self.image_y - self.border_width,
-                        image.width + (self.width - image.width) // 2 + self.border_width,
-                        self.image_y + self.border_width + image.height),
-                       fill=(255, 255, 255), outline=(0, 0, 0))
-        large_text_w, large_text_h = draw.textsize(text_prompt, font=self.large_font)
-        draw.text(((self.width - large_text_w) // 2 - 10,
-                   self.image_y + self.border_width + image.height),
-                  f"\"{text_prompt}\"", font=self.large_font, fill=(0, 0, 0))
+        # Add border
+        draw.rectangle(
+            (
+                (self.width - image.width) // 2 - self.border_width,
+                self.image_y - self.border_width,
+                image.width + (self.width - image.width) // 2 + self.border_width,
+                self.image_y + self.border_width + image.height
+            ),
+           fill=(255, 255, 255),
+           outline=(0, 0, 0)
+        )
 
-        small_text_w, small_text_h = draw.textsize(self.credit_text, font=self.small_font)
-        draw.text(((self.width - small_text_w) // 2,
-                   self.image_y + self.border_width + image.height + large_text_h),
-                  self.credit_text, font=self.small_font, fill=(150, 0, 0))
+        # add text prompt
+        large_text_w, large_text_h = draw.textsize(text_prompt, font=self.large_font)
+        draw.text(
+            (
+                (self.width - large_text_w) // 2 - 10,
+                self.image_y + self.border_width + image.height
+            ),
+            text=f"{text_prompt}",
+            font=self.large_font,
+            fill=(0, 0, 0)
+        )
 
         # add image to "frame"
-        base_image.paste(image, ((self.width - image.width) // 2,
-                                 self.image_y))
+        base_image.paste(
+            im=image,
+            box=((self.width - image.width) // 2, self.image_y)
+        )
 
         return base_image
