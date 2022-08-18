5da4dced21f92f3ea10696ae4d7ab7d14c0066d0
Palermo Penano <pspenano@outlook.com>
2022-08-18 02:20:21 +0000

    Increase max_result to 5 to comply with Twitter API minimum requirements; various formatting fix

--- client/tweets_utils.py
+++ client/tweets_utils.py
@@ -12,34 +12,34 @@ import tweepy
 import yaml
 
 TEXT_PROMPT_HASHTAG = "#dalle"
-MAX_NUM_TWEETS_TO_RETRIEVE = 3
+MAX_NUM_TWEETS_TO_RETRIEVE = 5
 
 
 def retrieve_tweets_containing_text_prompt(
-	client: tweepy.client.Client,
-	configs: Dict[str, str]
+    client: tweepy.client.Client,
+    configs: Dict[str, str]
 ) -> List[Tuple[int, str]]:
-	user_tweets = client.get_users_tweets(
+    user_tweets = client.get_users_tweets(
         id=configs["user_id"],
         max_results=MAX_NUM_TWEETS_TO_RETRIEVE
     )
-	return [(t.id, t.text) for t in user_tweets.data if TEXT_PROMPT_HASHTAG in t.text]
+    return [(t.id, t.text) for t in user_tweets.data if TEXT_PROMPT_HASHTAG in t.text]
 
 
 def remove_hashtags_and_mentions_from_tweet(tweet: str) -> str:
-	return re.sub("#[A-Za-z0-9_]+", "", re.sub("@[A-Za-z0-9_]+", "", tweet))
+    return re.sub("#[A-Za-z0-9_]+", "", re.sub("@[A-Za-z0-9_]+", "", tweet))
 
 
 def clean_up_tweets(tweets: List[str]) -> List[Tuple[int, str]]:
-	return [
+    return [
         (tweet[0], remove_hashtags_and_mentions_from_tweet(tweet[1]).strip())
         for tweet in tweets
-	]
+    ]
 
 
 def retrieve_most_recent_text_prompt(client: tweepy.client.Client, configs: Dict[str, str]):
-	raw_tweets = retrieve_tweets_containing_text_prompt(client=client, configs=configs)
-	if not raw_tweets:
-		raise ValueError(f"There are no text prompts containing the trigger hashtag {TEXT_PROMPT_HASHTAG}")
-	text_prompts = clean_up_tweets(raw_tweets)
-	return text_prompts[0]
+    raw_tweets = retrieve_tweets_containing_text_prompt(client=client, configs=configs)
+    if not raw_tweets:
+        raise ValueError(f"There are no text prompts containing the trigger hashtag {TEXT_PROMPT_HASHTAG}")
+    text_prompts = clean_up_tweets(raw_tweets)
+    return text_prompts[0]
