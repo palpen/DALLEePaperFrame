4bbe2407b192cce1638d1984b284f53189466b99
Palermo Macbook Air <pspenano@outlook.com>
2022-08-25 23:22:34 -0400

    Add config files for individual clients and load them when running the start_all_client.sh script

--- start_all_client.sh
+++ start_all_client.sh
@@ -1,25 +1,25 @@
 # Script that starts all clients passed in as arguments
-# Usage: ./start_all_client.sh user@host1 user@host2 user@host3
+# Usage: ./start_all_client.sh 1 2 3
 
 # Path to repository in each raspberry pi
 PATH_TO_REPO=/home/pi/DALLEePaperFrame
 SERVER_IP_ADDR="10.0.0.87"
 
-# Command to run inside a screen session
-main_cmd="
-    cd $PATH_TO_REPO/client && \
-    bash $PATH_TO_REPO/client/run_client.sh $SERVER_IP_ADDR $PATH_TO_REPO/config.yml ||
-    exec bash
-"
 
 # Command that starts a screen session in detached mode and runs $main_cmd
 screen_cmd="screen -dmS running_client bash -c \"$main_cmd\""
 
-for client in "$@"
+for client_num in "$@"
     do
-       echo "Running client in $client..."
-        # SSH into client, start a screen session,
-        # start the client, then exit the ssh session"
-        ssh $client "screen -XS running_client quit; $screen_cmd" &
+       echo "Running client in pi@raspberrypi$client_num.local ..."
+        # Command to run inside a screen session
+        main_cmd="
+            cd $PATH_TO_REPO/client && \
+            bash $PATH_TO_REPO/client/run_client.sh $SERVER_IP_ADDR $PATH_TO_REPO/config_rpi$client_num.yml ||
+            exec bash
+        "
+       # SSH into client, start a screen session,
+       # start the client, then exit the ssh session"
+       ssh pi@raspberrypi$client_num.local "screen -XS running_client quit; $screen_cmd" &
     done
 wait
