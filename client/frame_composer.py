620d7c8cba0b044428f09cdff91ccdf873c39322
Palermo Macbook Air <pspenano@outlook.com>
2022-08-26 23:03:01 -0400

    Implement feature that allows text prompt to be printed over two lines when image is in portrait mode

--- client/frame_composer.py
+++ client/frame_composer.py
@@ -1,6 +1,13 @@
 from PIL import Image, ImageDraw, ImageFont
 
 
+# MAX_CHARACTERS is the max number of characters that single line can have.
+# If the text prompt exceeds this value, then split the text prompt into two
+# lines with the first line having at most MAX_WORDS number of words.
+MAX_CHARACTERS = 40
+MAX_WORDS = 9
+
+
 class FrameComposer:
     def __init__(self, width, height):
 
@@ -10,8 +17,11 @@ class FrameComposer:
 
         # Text prompt properties
         self.large_font_height = 22
-        self.large_font = ImageFont.truetype("/usr/share/fonts/truetype/msttcorefonts/arial.ttf",
-                                             self.large_font_height)
+        self.large_font = ImageFont.truetype(
+            "/usr/share/fonts/truetype/msttcorefonts/arial.ttf",
+            self.large_font_height
+        )
+        self.second_line_offset = 30
 
         # Image border properties
         self.image_y = 13  # Vertical distance between top of display and top image border
@@ -22,12 +32,18 @@ class FrameComposer:
 
         To quickly iterate on different types of layouts, see frame_composer_test.py
         """
-        base_image = Image.new("RGB", (self.width, self.height), "white")
+        # If image is displayed in portrait mode, make it taller so that
+        # there's room for the second line of the text prompt if it's needed
+        base_image_height = (
+            self.height + self.second_line_offset if portrait_mode else self.height
+        )
+
+        base_image = Image.new("RGB", (self.width, base_image_height), "white")
         draw = ImageDraw.Draw(base_image)
 
-        # Add border
+        # Add border around image
         draw.rectangle(
-            (
+            xy=(
                 (self.width - image.width) // 2 - self.border_width,
                 self.image_y - self.border_width,
                 image.width + (self.width - image.width) // 2 + self.border_width,
@@ -37,24 +53,52 @@ class FrameComposer:
            outline=(0, 0, 0)
         )
 
-        # add text prompt
-        large_text_w, large_text_h = draw.textsize(text_prompt, font=self.large_font)
-        draw.text(
-            (
-                (self.width - large_text_w) // 2 - 10,
-                self.image_y + self.border_width + image.height
-            ),
-            text=f"{text_prompt}",
-            font=self.large_font,
-            fill=(0, 0, 0)
-        )
+        # Check if the text prompt is too long for the display. If it is
+        # then split it into two lines if the frame is also in portrait mode.
+        text_prompt_split = text_prompt.split(" ")
+        text_y = self.image_y + self.border_width + image.height
+        if len(text_prompt) > MAX_CHARACTERS and portrait_mode:
 
-        # add image to "frame"
+            first_line = " ".join(text_prompt_split[:MAX_WORDS])
+            second_line = " ".join(text_prompt_split[MAX_WORDS:])
+
+            large_text_w, _ = draw.textsize(first_line, font=self.large_font)
+            text_x = (self.width - large_text_w) // 2 - 10
+            draw.text(
+                xy=(text_x, text_y),
+                text=f"{first_line}",
+                font=self.large_font,
+                fill=(0, 0, 0)
+            )
+            draw.text(
+                xy=(text_x, text_y + 30),
+                text=f"{second_line}",
+                font=self.large_font,
+                fill=(0, 0, 0)
+            )
+        else:
+            large_text_w, _ = draw.textsize(text_prompt, font=self.large_font)
+            text_x = (self.width - large_text_w) // 2 - 10
+            draw.text(
+                xy=(text_x, text_y),
+                text=f"{text_prompt}",
+                font=self.large_font,
+                fill=(0, 0, 0)
+            )
+
+        # Add image to "frame"
         base_image.paste(
             im=image,
             box=((self.width - image.width) // 2, self.image_y)
         )
-
-        if portrait_mode:
-            return base_image.rotate(90, fillcolor="white")
-        return base_image
+        return (
+            base_image.
+                rotate(90, fillcolor="white").
+                resize(  # We resize to allow the final image to fit the ePaper display's dimensions
+                    (
+                        self.width,
+                        base_image_height - self.second_line_offset
+                    )
+                )
+            if portrait_mode else base_image
+        )
--- client/frame_composer_test.py
+++ client/frame_composer_test.py
@@ -4,10 +4,19 @@ Code for quickly experimenting with different display layouts
 
 from PIL import Image, ImageDraw, ImageFont
 
-text_prompt = "A park in Monet"
+text_prompt = "The quick brown fox jumped over the lazy dogs to cross the river"
+#text_prompt = "The quick brown fox jumped over"
+#text_prompt = "a cat in the hat saw a fat bat on a mat"
+#text_prompt = "A painting of a robot in the style of Van Gogh"
 
 # Display width and height
-width = 600; height = 448
+width = 600
+height = 448
+
+# Check length of text prompt
+max_characters = 40  # In case a text prompt contains short words, use number of characters to determine the split
+max_words = 7
+portrait_mode = True
 
 image_width = 390
 image_height = 390
@@ -17,6 +26,11 @@ image_y = 15  # Vertical distance from the top of display
 
 large_font_height = 25
 large_font = ImageFont.truetype("/Users/palermopenano/Downloads/Arial.ttf", large_font_height)
+
+# If image is displayed in portrait mode, make it taller so that
+# there's room for the second line of the text prompt if it's needed
+if portrait_mode:
+    height += 30
 
 base_image = Image.new("RGB", (width, height), "white")
 draw = ImageDraw.Draw(base_image)
@@ -29,19 +43,40 @@ rectangle_boundaries = (
 )
 draw.rectangle(rectangle_boundaries, fill=(255,255,255), outline=(0,0,0))
 
+text_prompt_split = text_prompt.split(" ")
+if len(text_prompt) > max_characters and portrait_mode:
 
-large_text_w, large_text_h = draw.textsize(text_prompt, font=large_font)
-draw.text(
-    (
-        (width - large_text_w) // 2 - 10,
-        image_y + border_width + image_height
-    ),
-    text=f"\"{text_prompt}\"",
-    font=large_font,
-    fill=(0, 0, 0)
-)
+    first_line = " ".join(text_prompt_split[:max_words])
+    second_line = " ".join(text_prompt_split[max_words:])
 
-#draw.multiline_text((10, 10), "Hello\nWorld", font=large_font, fill=(0, 0, 0))
-
-base_image.show()
+    large_text_w, _ = draw.textsize(first_line, font=large_font)
+    text_x = (width - large_text_w) // 2 - 10
+    text_y = image_y + border_width + image_height
+    draw.text(
+        xy=(text_x, text_y),
+        text=f"{first_line}",
+        font=large_font,
+        fill=(0, 0, 0)
+    )
+    draw.text(
+        xy=(text_x, text_y + 30),
+        text=f"{second_line}",
+        font=large_font,
+        fill=(0, 0, 0)
+    )
+    base_image = base_image.resize((600, 448))
+    base_image.show()
+    print(base_image.size)
+    base_image.rotate(90, fillcolor="white").show()
+else:
+    large_text_w, _ = draw.textsize(text_prompt, font=large_font)
+    text_x = (width - large_text_w) // 2 - 10
+    text_y = image_y + border_width + image_height
+    draw.text(
+        xy=(text_x, text_y),
+        text=f"{text_prompt}",
+        font=large_font,
+        fill=(0, 0, 0)
+    )
+    base_image.show()
 
