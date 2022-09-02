e69e2ce0316bde91860a7db81b3751ee839e5e86
Palermo Macbook Air <pspenano@outlook.com>
2022-09-02 11:57:22 -0400

    add vim and screen to install script

--- client/setup_client.sh
+++ client/setup_client.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/bash
-sudo apt-get install -y python3-pip python3-numpy libatlas-base-dev libopenjp2-7 libtiff5 ttf-mscorefonts-installer python3-dev portaudio19-dev libjpeg-dev zlib1g-dev
+sudo apt-get install -y python3-pip python3-numpy libatlas-base-dev libopenjp2-7 libtiff5 ttf-mscorefonts-installer python3-dev portaudio19-dev libjpeg-dev zlib1g-dev screen vim
 pip install -r requirements.txt
 
 curl -s https://get.pimoroni.com/inky | bash -s
